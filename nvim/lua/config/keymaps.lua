a8c5cdd7c2ebdffadca908995f4e9aff69f6ef9d
Gabrielle Jonathan Juanillo <gjuanillo.dev@gmail.com>
2025-10-14 22:58:00 +0800

    Nvim: Add Options and Keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,34 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+
+-- Set leader keybinding to space
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+--remove search highlights after searching
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>", { desc = "Remove search highlights" })
+
+-- Exit Terminal Mode
+vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+
+-- Disable Arrow keys
+vim.keymap.set("n", "<Up>", "<Nop>")
+vim.keymap.set("n", "<Down>", "<Nop>")
+vim.keymap.set("n", "<Left>", "<Nop>")
+vim.keymap.set("n", "<Right>", "<Nop>")
+
+-- Window Navigation Preferences
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+
+-- Split Windows
+vim.keymap.set("n", "<leader>wv", ":vsplit<cr>", { desc = "[W]indow Split [V]ertical" })
+vim.keymap.set("n", "<leader>wh", ":split<cr>", { desc = "[W]indow Split [H]orizontal" })
+
+-- Stay in indent mode
+vim.keymap.set("v", "<", "<gv", { desc = "Indent left in visual mode" })
+vim.keymap.set("v", ">", ">gv", { desc = "Indent right in visual mode" })
--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,4 +1,43 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.opt.relativenumber = false
+
+-- Left Column Related Configs
+vim.opt.number = true --line numbers
+vim.opt.relativenumber = true -- enable relative numbers
+vim.opt.numberwidth = 2 --set left column width to 2
+vim.opt.signcolumn = "yes" --show left signs "git status, diagnostics, etc"
+vim.opt.wrap = false
+vim.opt.scrolloff = 10
+vim.opt.sidescrolloff = 8
+
+-- Tab behavior
+vim.opt.expandtab = true --convert tabs to spaces
+vim.opt.shiftwidth = 4 --number of spaces for each indent
+vim.opt.tabstop = 4 --number of spaces for tab character
+vim.opt.softtabstop = 4 -- number of spaces inserted for tab key
+vim.opt.smartindent = true -- enable smart indentation
+vim.opt.breakindent = true -- enable line breaking indentation
+
+--Others
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+vim.opt.backup = false
+vim.opt.clipboard = "unnamedplus"
+vim.opt.conceallevel = 0
+vim.opt.fileencoding = "utf-8"
+vim.opt.mouse = "a"
+vim.opt.showmode = false
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.termguicolors = true
+vim.opt.timeoutlen = 2000
+vim.opt.undofile = true
+vim.opt.updatetime = 100
+vim.opt.writebackup = false
+vim.opt.cursorline = true
+
+-- Searching Configs
+vim.opt.hlsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
